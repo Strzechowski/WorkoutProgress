9c60cb5fe4c04037c70234f27cf4c62ba094776c
Strzechowski <rafal.strzechowski1@gmail.com>
2020-04-07 19:20:15 +0200

    Appending to lists added
    User can now easily add exercises to the list.
    At the end of a list there is a button that prompts for input for new exercise.
    Same option was added to occurences list.

--- src/components/Training.vue
+++ src/components/Training.vue
@@ -7,7 +7,7 @@
         <span @click="listPopUp(exercise.id)">
           #{{exercise.id}} {{exercise.name}}
         </span>
-        <ol class="ol-inside" v-if="exercise.open === true">
+        <ol class="ol-inside" v-if="editing === exercise.id">
           <li  class="li-inside" v-for="occurence in exercise.occurences" v-bind:key="occurence.id">
             <div v-if="editing === exercise.id && editingOccurence === occurence.id">
               <div class="div-in-row">
@@ -24,7 +24,35 @@
               {{occurence.date}} -> {{occurence.weight}}kg
             </div>
           </li>
+          <li class="li-inside">
+            <div @click="startAddingOccurence()">
+              + New occurence
+            </div>
+            <div v-if="editing === exercise.id && editingOccurence === 'addOccurence'">
+              <div class="div-in-row">
+                <label>Date:</label>
+                <input type="text" v-model="newDate">
+              </div>
+              <div class="div-in-row">
+                <label>Weight:</label>
+                <input type="text" v-model="newWeight">
+              </div>
+              <button @click="addNewOccurence(exercise.id)">ADD</button>
+            </div>
+          </li>
         </ol>
+      </li>
+      <li>
+        <span @click="startAddingExercise()">
+          + New exercise
+        </span>
+        <div v-if="editing === 'addExercise'">
+          <div class="div-in-row">
+            <label>Name:</label>
+            <input type="text" v-model="newName">
+          </div>
+          <button @click="addNewExercise()">ADD</button>
+        </div>
       </li>
     </ol>
   </div>
@@ -39,7 +67,10 @@ export default {
       clicked: [0, 0, 0],
       exercises: [],
       editing: 0,
-      editingOccurence: 0
+      editingOccurence: 0,
+      newName: 'Exercise',
+      newDate: '01.01.2020',
+      newWeight: 0
     }
   },
 
@@ -124,7 +155,7 @@ export default {
     },
 
     listPopUp(id) {
-      this.exercises[id - 1].open = !this.exercises[id - 1].open
+      this.editing == id ? this.editing = 0 : this.editing = id
     },
 
     change(id, occurenceId) {
@@ -133,9 +164,38 @@ export default {
     },
 
     save() {
-      this.editing = 0
       this.editingOccurence = 0
-    }
+    },
+
+    startAddingExercise() {
+      this.editing == 'addExercise' ? this.editing = 0 : this.editing = 'addExercise'
+    },
+
+    addNewExercise() {
+      var newId = this.exercises.length
+      var newExercise = {
+        "id": newId + 1,
+        "name": this.newName,
+        "occurences": []
+      }
+      this.exercises[newId] = newExercise
+      this.editing = 0
+    },
+
+    startAddingOccurence() {
+      this.editingOccurence == 'addOccurence' ? this.editingOccurence = 0 : this.editingOccurence = 'addOccurence'
+    },
+
+    addNewOccurence(id) {
+      var newId = this.exercises[id - 1].occurences.length
+      var newOccurence = {
+        "id": newId + 1,
+        "date": this.newDate,
+        "weight": this.newWeight
+        }
+      this.exercises[id - 1].occurences[newId] = newOccurence
+      this.editingOccurence = 0
+    },
 
   }
 }
